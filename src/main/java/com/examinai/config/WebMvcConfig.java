aac7e970556b369e2280ea809cc9135402395a18
Ulugbek <mr.great_95@mail.ru>
2026-04-22 12:34:33 +0500

    Successfully ran the application in Docker container along DB and AI services

--- src/main/java/com/examinai/config/WebMvcConfig.java
+++ src/main/java/com/examinai/config/WebMvcConfig.java
@@ -2,10 +2,16 @@ package com.examinai.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("auth/login");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
